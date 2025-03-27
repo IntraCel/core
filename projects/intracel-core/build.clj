b3844b8088e0f421ce579b0fcae0bac1fdba21c8
Jared Holmberg <jaredholmberg@hotmail.com>
2025-03-27 07:35:14 -0500

    Issue #12 - Getting a jar to build but seems like the classes are empty. Just metadata in tere.

--- projects/intracel-core/build.clj
+++ projects/intracel-core/build.clj
@@ -14,12 +14,16 @@
   (prn "Cleaned projects/intracel-core/target"))
 
 (defn jar [_]
+  (clean nil)
+  
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  
   (b/write-pom {:class-dir class-dir
                 :lib lib
                 :version version
                 :basis @basis
                 :src-dirs ["src"]})
-  (b/copy-dir {:src-dirs ["src" "resources"]
-               :target-dir class-dir})
+  
   (b/jar {:class-dir class-dir
           :jar-file jar-file}))
