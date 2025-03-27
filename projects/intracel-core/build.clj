10db8bb6c145b52b71e61b7af5e28b3b3a97f7c9
Jared Holmberg <jaredholmberg@hotmail.com>
2025-03-27 16:45:10 -0500

    Issue #12 - Building an uberjar now. One step closer. Now need to figure out how to set up a github action that will publish to clojars.

--- projects/intracel-core/build.clj
+++ projects/intracel-core/build.clj
@@ -5,6 +5,9 @@
 (def version (format "0.1.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+
+
 
 ;; delay to defer side effects (artifact downloads)
 (def basis (delay (b/create-basis {:project "deps.edn"})))
@@ -16,8 +19,12 @@
 (defn jar [_]
   (clean nil)
   
-  (b/copy-dir {:src-dirs ["src" "resources"]
+  (b/copy-dir {:src-dirs ["src" "resources" "components" "bases"]
                :target-dir class-dir})
+  (b/compile-clj {:basis @basis
+                  :src-dirs ["src" "resources" "components" "bases"]
+                  :class-dir class-dir})
+
   
   (b/write-pom {:class-dir class-dir
                 :lib lib
@@ -27,3 +34,16 @@
   
   (b/jar {:class-dir class-dir
           :jar-file jar-file}))
+
+(defn uberjar [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "components" "bases"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis @basis
+                  :src-dirs ["src" "components" "bases"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis @basis
+           ;;:main 'your-main.namespace
+           }))
